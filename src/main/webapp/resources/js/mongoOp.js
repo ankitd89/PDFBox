ecc8864ea3de403e7c678a2a82711b9bc89407ae
tchaubal <tanushree2492@gmail.com>
2014-12-09 21:27:52 -0800

    Validated all Fields and displayed using modal dialogues.

--- src/main/webapp/resources/js/mongoOp.js
+++ src/main/webapp/resources/js/mongoOp.js
@@ -6,6 +6,14 @@ function getEarningOnPaymentType()
 	var selectId =document.getElementById("cboPayment");
 	var type=selectId.options[selectId.selectedIndex].value;
 	var paymentUrl = "/dropbox/"+email+"/getEarningsUponPaymentType/" + type;
+
+			if(document.getElementById("cboPayment").selectedIndex=="0")
+			{
+			 var header="ERROR";
+             var msg="Please Select Payment Option!"
+             alertMessage(header,msg); 
+             return;
+            }
 	$.ajax({
 		type: "GET",
 		contentType: "application/json",
@@ -31,6 +39,14 @@ function getBillsOnCondition()
 	var amtId = document.getElementById("txtAmount");
 	var amt = amtId.value;
 	var billCondUrl = url + "/dropbox/" + email + "/getBillsOnCondition/" + cnd +"/" + amt;
+
+	if(document.getElementById("txtAmount").value=='' || document.getElementById("cboAmount").selectedIndex=="0")
+			{
+			 var header="ERROR";
+             var msg="Required Fields Cannot Be Empty!"
+             alertMessage(header,msg); 
+             return;
+            }
 	$.ajax({
 		type: "GET",
 		contentType: "application/json",
@@ -52,6 +68,15 @@ function getBillsForDate()
 	var dateId = document.getElementById("txtDate"); 
 	var date = dateId.value;
 	var billUrl = url + "/dropbox/" + email + "/getBillsForDate/" + date;
+
+if(document.getElementById("txtDate").value=='')
+			{
+			 var header="ERROR";
+             var msg="Please Select Date!"
+             alertMessage(header,msg); 
+             return;
+            }
+
 	$.ajax({
 		type: "GET",
 		contentType: "application/json",
