bc5ab9a6fc18af059a2b0e3d3c65c2de27fba567
tchaubal <tanushree2492@gmail.com>
2014-12-10 16:03:01 -0800

    Modified upload validation.

--- src/main/webapp/resources/js/upload.js
+++ src/main/webapp/resources/js/upload.js
@@ -48,7 +48,46 @@ var fup = document.getElementById('file2');
        alertMessage(header,msg);  
         return ;
     }
-     
+		
+    $('#result').html('');
+  var oMyForm = new FormData();
+  oMyForm.append("file", file2.files[0]);
+  var hitUrl = staticUrl + "/dropbox/" + email + "/upload";
+
+  $.ajax({
+    
+    url: hitUrl,
+    data: oMyForm,
+    dataType: "text",
+    processData: false,
+    contentType: false,
+    type: "POST",
+    
+   
+    success: function(data){
+      $('#result').html(data);
+      listAllFiles();
+
+      var filediv=document.getElementById("file_div");
+       filediv.innerHTML="<input id='file2' name='file2' class='form-control' placeholder='No file selected' type='file' style='position: absolute; left: 10px; top: 0px;'>"
+    	  +"<div class='input-group-btn' style='position: relative; left: 50px; top: 0px; width: 150px; height: 50px'><button type='button' class='btn btn-info'"
+			+" onclick='uploadFormData()' style='position: absolute; left: 120px; top: 0px;'><span class='glyphicon glyphicon-arrow-up'></span> Upload</button></div>";
+     //var ext = fileName.substring(fileName.lastIndexOf('.') + 1);
+     /*if(ext =="pdf")
+    {
+     var header="Awesome";
+     var msg="Upload Successful!!!!"
+       alertMessage(header,msg);      
+	return;
+    }
+    else
+    {
+      var header="ERROR";
+     var msg="Please Enter A pdf File Only!"
+       alertMessage(header,msg);  
+        return ;
+    }*/
+    
     },
   error: function(jqXHR, status, errorThrown){
 	  var header="ERROR";
